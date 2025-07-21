dd42a13b7245aed02ffc3ed8be2e16d53a480f37
RamyGarici <ramymanilgarici@gmail.com>
2025-07-21 17:31:09 +0100

    test: Learning collisions with pygame

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import sys
 
 
 class Game:
+
     def __init__(self):
         
 
@@ -13,12 +14,22 @@ class Game:
         self.clock=pygame.time.Clock()
         self.img=pygame.image.load("data/images/clouds/cloud_1.png")
         
-        self.img_pos=[160,160]
+        self.img_pos=[160,260]
         self.movement=[False,False]
+        self.collision_area=pygame.Rect(50,50,300,50)
         
+    
     def run(self):
         while True:
+            self.screen.fill((14,219,248))
+            self.img.set_colorkey((0,0,0))
+            self.img_pos[1] += self.movement[1]-self.movement[0]
             self.screen.blit(self.img, self.img_pos)
+            img_r=pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen,(0,100,255),self.collision_area)
+            else:
+                pygame.draw.rect(self.screen,(0,50,155),self.collision_area)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -28,8 +39,17 @@ class Game:
                     if event.key == pygame.K_UP:
                         self.movement[0] = True
                     if event.key == pygame.K_DOWN:
-                        self.movement[1] = True
+                        self.movement[1] = True 
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+                        
+                        
             pygame.display.update()
             self.clock.tick(60)
+            
+            
             
 Game().run()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ self.img_pos[1] += self.movement[1]-self.movement[0]
+            self.screen.blit(self.img, self.img_pos)
